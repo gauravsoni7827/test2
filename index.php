5eab777b9647f21deba03bf6b861aa790d8e47ad
gaurav <gaurav.soni@thriwe.com>
2022-12-11 14:07:22 +0530

    sdsd

--- index.php
+++ index.php
@@ -5,28 +5,28 @@ require './aws/aws-autoloader.php';
 use Aws\S3\S3Client;
 use Aws\Exception\AwsException;
 
-$client=new Aws\S3\S3Client([
+ 
+$bucket = "mybucketthriwe";
+$file = "keyFile.json";
+$client = new Aws\S3\S3Client([
     'region'  => 'us-east-1',
     'version' => 'latest',
     'credentials' => [
         'key'    => "AKIAQ4XTBL7KY7GDV2VR",
         'secret' => "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",
     ]
-    ]);
+]);
 $result = $client->getObject(array(
-    'Bucket' => "mybucketthriwe",
-    'Key'    => 'key.json'
+    'Bucket' => $bucket,
+    'Key'    => $file,
+    'ContentType' => 'application/json',
 ));
 
+$json = $result['Body'];
 
-print_r($result);
+$object = json_decode($json, true);
 
-// The 'Body' value of the result is an EntityBody object
-// echo get_class($result['Body']) . "\n";
-// > Guzzle\Http\EntityBody
-
-// The 'Body' value can be cast to a string
-echo $result['Body'] . "\n";
+print_r($object);
 echo "wowww1111w<br>";
 echo "  QWQEKJHsfgb";
 echo "  QWQEK1233432JHsfgb";
