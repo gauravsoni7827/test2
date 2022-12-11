ea8730af43feb7ad793535bede02473ef298716e
gaurav <gaurav.soni@thriwe.com>
2022-12-11 16:13:14 +0530

    dwd

--- index.php
+++ index.php
@@ -16,17 +16,23 @@ $client = new Aws\S3\S3Client([
     //     'secret' => "VxhhUxg3PRxhE2lb7S6pfargfPPh53JBJlZ7h76l",
     // ]
 ]);
-$result = $client->getObject(array(
-    'Bucket' => $bucket,
-    'Key'    => $file,
-    'ContentType' => 'application/json',
-));
+// $result = $client->getObject(array(
+//     'Bucket' => $bucket,
+//     'Key'    => $file,
+//     'ContentType' => 'application/json',
+// ));
 
+$result = $client->putObject(array(
+    'Bucket' => $bucket,
+    'Key'    => "index",
+    // 'ContentType' => 'application/json',
+    'SourceFile' => './index.php',
+));
+print_r($object);die;
 $json = $result['Body'];
 
 $object = json_decode($json, true);
 echo "<pre>";
-print_r($object);
 echo "wowww1111w<br>";
 echo "  QWQEKJHsfgb";
 echo "  QWQEK1233432JHsfgb";
